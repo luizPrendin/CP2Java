b51c36502d57f742bf86a942db0907a4d73a4d9c
luizPrendin <luizprendinfelipe@gmail.com>
2024-04-16 11:48:03 -0300

    Add projeto base

---
+++ mini-spotify-552475/src/main/java/br/com/fiap/jadv/spotify5/minispotify552475/MiniSpotify552475Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.jadv.spotify5.minispotify552475;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSpotify552475Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniSpotify552475Application.class, args);
+	}
+
+}
---
+++ mini-spotify-552475/src/test/java/br/com/fiap/jadv/spotify5/minispotify552475/MiniSpotify552475ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.jadv.spotify5.minispotify552475;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSpotify552475ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
